fde899b7edabc838f3be60054eacb2ae668d64a4
john-oberhauser <john.oberhauser@live.com>
2016-07-23 18:55:01 -0500

    saving

--- convenience.js
+++ convenience.js
@@ -1,3 +1,8 @@
+const Gio = imports.gi.Gio;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
 const TopBarIcon = new Gio.FileIcon({
     file: Gio.File.new_for_path(Me.dir.get_child(
         'images/default_icon.svg').get_path())
--- extension.js
+++ extension.js
@@ -2,14 +2,17 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
+const PanelMenu = imports.ui.panelMenu;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Lib = Me.imports.convenience
+const Lib = Me.imports.convenience;
 
 let text, button;
+let indicator;
 
-function _hideHello() {
+/*function _hideHello() {
     Main.uiGroup.remove_actor(text);
     text = null;
 }
@@ -46,12 +49,79 @@ function init() {
 
     button.set_child(icon);
     button.connect('button-press-event', _showHello);
+}*/
+
+const AVS_Indicator = new Lang.Class({
+    Name: 'AVS.Indicator',
+    Extends: PanelMenu.Button,
+
+    _init: function () {
+        this.parent(null, 'AVS-indicator');
+
+        this.actor.connect(
+            'enter_event', Lang.bind(this, this.refreshIndicator, true));
+        this.actor.connect(
+            'leave_event', Lang.bind(this, this.refreshIndicator, false));
+        this.actor.connect(
+            'button_press_event', Lang.bind(this, this._onButtonPress, false));
+
+        this.indicatorBox = new St.BoxLayout;
+        this.indicatorIcon = new St.Icon({
+            gicon: Lib.TopBarIcon,
+            icon_size: 16
+        });
+        this.indicatorBox.add_actor(this.indicatorIcon);
+        this.actor.add_actor(this.indicatorBox);
+    },
+
+    _onButtonPress: function(actor, event) {
+        let button = event.get_button();
+
+        if (button === 1) {
+            _showMenu();
+        }
+    },
+
+    _showMenu: function(){
+        if (this.visibleMenu === null || this.visibleMenu === undefined) {
+            this.visibleMenu = new PopupMenu.PopupSubMenuMenuItem(
+                _('test'), true);
+        }else{
+            this.visibleMenu.menu.removeAll();
+        }
+
+        this.menu.addMenuItem(this.visibleMenu);
+    }
+
+    _enable: function() {
+        this.actor.add_actor(this.indicatorBox);
+    },
+
+    _disable: function() {
+        this.actor.remove_actor(this.indicatorBox);
+    }
+});
+
+function init(){
+    indicator = new AVS_Indicator();
+    Main.panel.addToStatusArea('AVS-indicator', indicator);
+    indicator._enable();
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    //Main.panel._rightBox.insert_child_at_index(button, 0);
+    if (indicator === null || indicator === undefined) {
+        indicator = new AVS_Indicator();
+        Main.panel.addToStatusArea('AVS-indicator', indicator);
+    }
+
+    indicator._enable();
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    //Main.panel._rightBox.remove_child(button);
+    if (indicator !== null) {
+        indicator._disable();
+        indicator = null;
+    }
 }
